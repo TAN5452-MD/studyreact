4b8e16f69bf742792aef36bfca6922b634424314
Your Name <545243476@qq.com>
2022-07-04 16:15:51 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
 /* import zhCN from 'antd/lib/locale/zh_CN'; */
 import moment from 'moment';
