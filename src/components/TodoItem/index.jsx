3ba5d3acd6eb28527d9b03dc67807203061dff6c
Your Name <545243476@qq.com>
2022-07-04 16:14:15 +0800

    1

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,17 +2,18 @@ import './index.css'
 
 export default function index(props) {
 
-  const date = props.todos[0].date.toLocaleDateString()
+  const date = props.todos[0].date
+  console.log(props.children);
   console.log(date)
   return (
      props.todos.map((item,index)=>
     <div key={index} style={{marginTop:'20px'}}>
         <div className='month'>
-            十月
+            {date.getMonth()}月
         </div>
         <div className='day'
         >
-          20
+          {date.getDay()}
     </div>
     <div className='item'>
       <p>{item.todo}</p>
